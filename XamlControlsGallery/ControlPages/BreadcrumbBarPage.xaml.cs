89e66bca0414038f386f45622f3347f60ff69b58
Yulia Klein <yulikl@microsoft.com>
2021-02-26 15:48:15 -0800

    Updates

--- XamlControlsGallery/ControlPages/BreadcrumbBarPage.xaml.cs
+++ XamlControlsGallery/ControlPages/BreadcrumbBarPage.xaml.cs
@@ -3,10 +3,10 @@ using Windows.UI.Xaml.Controls;
 
 namespace AppUIBasics.ControlPages
 {
-    public sealed partial class BreadcrumbPage : Page
+    public sealed partial class BreadcrumbBarPage : Page
     {
  
-    public BreadcrumbPage()
+    public BreadcrumbBarPage()
         {
             this.InitializeComponent();
             Breadcrumb1.ItemsSource = new string[] { "Home", "Documents", "Design", "Northwind", "Images" };
